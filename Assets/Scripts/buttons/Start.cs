8998ea02a19cd4181248330306223a1a2370360b
Amoral9642 <78478374+Amoral9642@users.noreply.github.com>
2024-04-23 15:20:33 -0400

    Add files via upload

---
+++ Assets/Scripts/buttons/Start.cs
@@ -0,0 +1,11 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Replace "MainScene" with the name of your main game scene
+    }
+}
